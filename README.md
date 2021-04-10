1e014d21aa3a0463c5875e3bf855f0716cc07371
Rob Campbell <rob@campbellzoo.com>
2021-04-09 21:29:13 -0500

    Add github URL to README

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ I felt they over complicated things and required so much clicky clicky to get to
 This is just an idea at this time and I hope to make it into a full project.
 
 This will be writting in PHP/MySQL
+
+Find the project at https://github.com/flattery103/SimpleMSPDoc
--- index.php
+++ index.php
@@ -1,1 +1,59 @@
-The main PHP index.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Your page title here :)</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+    <img class="logo" src='MSPDoc.png'>
+
+
+    <nav class="navbar">
+        <ul class="navbar-list">
+          <li class="navbar-item"><a class="navbar-link" href="#intro">Dashboard</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#intro">Companies</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#intro">Knowledge</a></li>
+	</ul>
+   </nav>
+
+
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="container">
+    <div class="row">
+      <div class="one-half column" style="margin-top: 25%">
+        <h4>Basic Page</h4>
+        <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
+      </div>
+    </div>
+  </div>
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
