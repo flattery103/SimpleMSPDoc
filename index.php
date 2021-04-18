f4d77085551795f2e814c9546dc8cc3fda44f90b
Rob Campbell <rob@campbellzoo.com>
2021-04-17 20:49:41 -0500

    minor bug fix

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 <?php
 
-if(isset($_GET['p']){
+if(isset($_GET['p'])){
 	if($_GET['p']=="companies") {
 		include('companies.php');
 	}elseif($_GET['p']=="knowledge") {
