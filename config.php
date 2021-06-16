a1776e4988f5f295018563a836c1091dad4b690c
Rob Campbell <rob@campbellzoo.com>
2021-06-15 22:27:44 -0500

    defaults

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-	$host = "localhost";
-        $user = "mspdoc";
-        $pass = "m5lQxx7FFlNJTuZy";
-        $dbname = "mspdoc";
+	$host = "CHANGEME!";
+        $user = "CHANGEME!";
+        $pass = "CHANGEME!";
+        $dbname = "CHANGEME!";
 
 	//This directory is where your files are uploaded and is good to have outside of the web space for security.
-	$file_upload_dir = "/www/mySites/mspdoc.net/devfiles/";
+	$file_upload_dir = "/path/to/files-CHANGEME/";
 ?>
