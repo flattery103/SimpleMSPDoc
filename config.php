8463330546075f557b0132737da7d73c406e46f9
Rob Campbell <rob@campbellzoo.com>
2021-06-15 19:21:37 -0500

    Fixed download bug

--- config.php
+++ config.php
@@ -4,4 +4,6 @@
         $pass = "CHANGEME!";
         $dbname = "CHANGEME!";
 
+	//This directory is where your files are uploaded and is good to have outside of the web space for security.
+	$file_upload_dir = "/path/to/files-CHANGEME/";
 ?>
--- files.php
+++ files.php
@@ -5,19 +5,20 @@ include('config.php');
 
 if(isset($_GET['f'])){
 	//File download
-	//BUG: Only seems to work on basic text files.
 	if(file_exists($file_upload_dir."/".$_GET['c']."/".$file)){
 		$_GET['f']=stripslashes(str_replace( '/', '', $_GET['f'] ));
-		$file = $_GET['f'] ?: '.';
-		$filename = basename($file);
-		$finfo = finfo_open(FILEINFO_MIME_TYPE);
-
-		header('Content-Type: ' . finfo_file($finfo, $file));
-		header('Content-Length: '. filesize($file));
-		header(sprintf('Content-Disposition: attachment; filename=%s',
-			strpos('MSIE',$_SERVER['HTTP_REFERER']) ? rawurlencode($filename) : "\"$filename\"" ));
-		ob_flush();
-		readfile($file_upload_dir."/".$_GET['c']."/".$file);
+		$file=$file_upload_dir."/".$_GET['c']."/".$_GET['f'];
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename=' . basename($file));
+		header('Content-Transfer-Encoding: binary');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($file));
+		ob_clean();
+		flush();
+		readfile($file);
 	}
 	exit;
 }
@@ -68,6 +69,8 @@ if(isset($_GET['a'])){
 	$dir = scandir($file_upload_dir."/".$_GET['c']."/");
 	//print_r($dir);
 	for($i=2;$i<sizeof($dir);$i++){
+		//TODO: Provide preview link for txt and images
+		//TODO: Provide delete button
 		echo '<tr><td></td><td>'.$dir[$i].'</td><td><a href="files.php?c='.$_GET['c'].'&f='.$dir[$i].'" target="_blank">Download</a> </td></tr>';
 	}
 }
